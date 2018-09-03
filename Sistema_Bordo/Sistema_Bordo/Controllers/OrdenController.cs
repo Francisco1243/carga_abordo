09d0ece7522b63bad7a1f1d25b61f2ce2a00dd2b
Centro de Desarrollo De Software <cds@univo.edu.sv>
2018-09-03 09:53:44 -0600

    cambio2

---
+++ Sistema_Bordo/Sistema_Bordo/Controllers/OrdenController.cs
@@ -0,0 +1,53 @@
+﻿using Sistema_Bordo.Models;
+using Sistema_Bordo.Models.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_Bordo.Controllers
+{
+    public class OrdenController : Controller
+    {
+        DB_Carga db = new DB_Carga();
+
+        // GET: Orden
+        public ActionResult NuevaOrden()
+        {
+            var Ordenview = new OrdenViews();
+            Ordenview.Empleados = new Empleados();
+            Ordenview.Productos = new List<OrdenProductos>();
+            //esta es el viewbag para mandarle la lista empleados que tenemos en la base de datos pero 
+            //hay que ordenarla
+            var lista = db.Empleados.ToList();
+            lista.Add(new Empleados { Id_empleado = 0, Nombres = "[ Seleccione un Empleado.....]" });
+            lista = lista.OrderBy(c => c.Nombres);
+            ViewBag.Empleado = new SelectList(lista, "Id_empleado", "Nombre");
+            return View(Ordenview);
+        }
+
+        [HttpPost]
+        public ActionResult NuevaOrden(OrdenViews ordenView)
+        {
+            var Ordenview = new OrdenViews();
+            Ordenview.Empleados = new Empleados();
+            Ordenview.Productos = new List<OrdenProductos>();
+            //esta es el viewbag para mandarle la lista empleados que tenemos en la base de datos pero 
+            //hay que ordenarla
+            var lista = db.Empleado.ToList();
+            lista.Add(new Empleados { Id_empleado = 0, Nombres = "[ Seleccione un Empleado.....]" });
+            lista = lista.OrderBy(c => c.Nombre);
+            ViewBag.Empleado = new SelectList(lista, "Id_empleado", "Nombre");
+
+            return View(Ordenview);
+        }
+
+        public ActionResult CargarProducto(OrdenProductos ordenProductos)
+        {
+            return View(ordenProductos);
+        }
+        //colocar en esta parte el metodo conde serramos la base de datos
+
+    }
+}
